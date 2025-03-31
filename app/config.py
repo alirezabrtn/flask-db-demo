d1f477257858beaf99e64447e3a0262d158fc688
Alireza Baratian <alireza.baratian@gmail.com>
2025-03-31 11:25:23 +0330

    Fix password issue

--- app/config.py
+++ app/config.py
@@ -7,5 +7,5 @@ class Config(object):
     """Base Config Object"""
     DEBUG = os.environ.get('FLASK_DEBUG', False)
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://yourusername:yourpassword@localhost/databasename')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://flask-db:flask-db@127.0.0.1/flask-db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from . import db
-# from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash
 
 
 class User(db.Model):
@@ -9,12 +9,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    #password = db.Column(db.String(255))
+    password = db.Column(db.String(255))
 
-    def __init__(self, username, email):
+    def __init__(self, username, email, password):
         self.username = username
         self.email = email
-        #self.password = generate_password_hash(password)
+        self.password = generate_password_hash(password)
 
     def __repr__(self):
         return '<User %r>' % self.username
