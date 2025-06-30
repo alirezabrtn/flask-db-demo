d2c808d1eb497d0c0f63a1fb30661bc83f6a64c0
Alireza Baratian <alireza.baratian@gmail.com>
2025-06-30 15:19:27 +0330

    Add password to database - Added password definition to user model - Defined a flask migration

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    #password = db.Column(db.String(255))
+    password = db.Column(db.String(255))
 
     def __init__(self, username, email, password):
         self.username = username
